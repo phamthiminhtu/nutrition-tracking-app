787320ef218a2ec3c156f2509a1f2c31e7d115ea
tyl <nyanpaing.htun-1@student.uts.edu.au>
2024-03-23 21:15:27 +1100

    fixed registeration

--- test_auth.py
+++ test_auth.py
@@ -9,6 +9,7 @@ from core.duckdb_connector import *
 
 st.set_page_config(layout='wide')
 
+
 user_profiles_temp = pd.DataFrame(
     [
         {
@@ -27,25 +28,32 @@ user_profiles_temp = pd.DataFrame(
         }
     ]
 )
+#print(user_profiles_temp['user_id'])
 
 db = DuckdbConnector()
+#user_profiles_temp = db.fetch_users()
+#print(user_profiles_temp)
 auth = Authenticator()
 #db.create_users_table()
-#res = db.insert_user({
-#            "user_id": "tu@gmail.com",
-#            "gender": "female",
-#            "age": 20.0,
-#            "username": "Tu",
-#            "password": "Protein"
-#        })
-#db.insert_user({
-#            "user_id": "nyan@gmail.com",
-#            "gender": "male",
-#            "age": 26,
-#            "username": "Tyler",
-#            "password": "Vitamin A"
-#        })
+inse=False
+if inse!=True:
+    #db.create_users_table()
+    #res = db.insert_user({"user_id": "nyan@gmail.com","gender": "male","age": 26,"username": "Tyler","password": "Vitamin A"})
+    #res2 = db.insert_user({"user_id": "tu@gmail.com","gender": "female","age": 20.0,"username": "Tu","password": "Protein"})
+    inse=True
 #print(res)
 #print(db.fetch_users())
-auth.log_in()
-#auth.register_user()
+print(auth.log_in())
+#auth.register_user_form()
+auth.log_out()
+if 'button' not in st.session_state:
+    st.session_state.button = False
+
+def click_button():
+    st.session_state.button = not st.session_state.button
+
+st.button('Register new user', on_click=click_button)
+
+if st.session_state.button:
+    # The message and nested widget will remain on the page
+    auth.register_user_form()
