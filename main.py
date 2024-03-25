469f7645754948953a5c6e6e7119e1f62460fe36
tu.pham <you@example.com>
2024-03-26 07:42:45 +1100

    fix full reloading bug with session_state

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ st.set_page_config(layout='wide')
 # Authenticator.create_new_account()
 
 ### TODO: replace this with actual input
-st.session_state['is_logged_in'] = False
-st.session_state['user_name'] = None
+st.session_state['is_logged_in'] = True
+st.session_state['user_name'] = "Tu"
 # user_name = None
 # user_id = "abc"
-st.session_state['user_id'] = "tu@gmail.com"
+st.session_state['user_id'] = "tu_2@gmail.com"
 ###
 
 
@@ -56,7 +56,6 @@ dish_description = track_new_meal_tab.text_input("What have you eaten today? �
 if 'dish_description' not in st.session_state and dish_description!= '':
     logging.info("-----------Running get_user_input_dish_and_estimate_ingredients()-----------")
     st.session_state['dish_description'] = dish_description
-    print("######", st.session_state.get('dish_description') )
 
 event = threading.Event()
 while 'dish_description' not in st.session_state:
@@ -72,16 +71,17 @@ if 'ingredient_df' not in st.session_state:
 while 'ingredient_df' not in st.session_state:
     event.wait()
 
-track_new_meal_tab.write(f'Here is our estimated weight of each ingredient for one serving of 🍕 {st.session_state["dish_description"]} 🍳:')
-track_new_meal_tab.write(st.session_state['ingredient_df'])
 
-if 'ingredient_df' in st.session_state:
+if 'ingredient_df' in st.session_state and 'confirm_ingredient_weights_button' not in st.session_state:
     track_new_meal_tab.write("Press continue to get your nutrition estimation...")
     logging.info("-----------Finished get_user_input_dish_and_estimate_ingredients.-----------")
+    confirm_ingredient_weights_button = track_new_meal_tab.button(label="Continue", key="confirm_ingredient_weights")
+    if confirm_ingredient_weights_button:
+        st.session_state['confirm_ingredient_weights_button'] = confirm_ingredient_weights_button
 
-    event = threading.Event()
-    while not track_new_meal_tab.button(label="Continue", key="confirm_ingredient_weights"):
-        event.wait()
+event = threading.Event()
+while not st.session_state.get('confirm_ingredient_weights_button', False):
+    event.wait()
 
 # # 2-3. Nutrient actual intake
 # # wait until user inputs
@@ -110,18 +110,14 @@ user_intake_df_temp = st.session_state['total_nutrients_based_on_food_intake']
 # # @Tu
 if 'user_personal_data' not in st.session_state:
     logging.info("----------- Running get_user_personal_data()-----------")
-
     user_personal_data = main_app_miscellaneous.get_user_personal_data(
         is_logged_in=st.session_state['is_logged_in'],
         user_id=st.session_state['user_id'],
-        has_user_intake_df_temp_empty=[
-            user_intake_df_temp.empty if isinstance(user_intake_df_temp, pd.DataFrame) else True
-        ][0],   ## handle case total_nutrients_based_on_food_intake is not a DataFrame but a dict
+        has_user_intake_df_temp_empty=user_intake_df_temp.empty if isinstance(user_intake_df_temp, pd.DataFrame) else True,   ## handle case total_nutrients_based_on_food_intake is not a DataFrame but a dict
         layout_position=track_new_meal_tab
     )
     st.session_state['user_personal_data'] = user_personal_data
     logging.info("-----------Finished get_user_personal_data-----------")
-
 
 # 6. Join with recommended intake
 # Only run when we have user_personal_data
@@ -139,31 +135,37 @@ if 'user_recommended_intake_result' not in st.session_state:
 user_recommended_intake_df = st.session_state['user_recommended_intake_result'].get("value")
 logging.info("-----------Finished combine_and_show_users_recommended_intake-----------")
 
-# 6. @Michael
-# Visualize data
+# # 6. @Michael
+# # Visualize data
 
-st.write(st.session_state['user_recommended_intake_result'])
 logging.info("-----------Running get_user_confirmation_and_try_to_save_their_data()-----------")
-if st.session_state['user_recommended_intake_result'].get("status") == 200:
+if st.session_state['user_recommended_intake_result'].get("status") == 200 and 'save_meal_result' not in st.session_state:
 
     ### TODO: replace this with actual input
     # is_logged_in = True
-    user_recommended_intake_df["user_id"] = st.session_state['user_id']
+    user_intake_df_temp["user_id"] = st.session_state['user_id']
     ###
 
-    result = main_app_miscellaneous.get_user_confirmation_and_try_to_save_their_data(
+    save_meal_result = main_app_miscellaneous.get_user_confirmation_and_try_to_save_their_data(
         dish_description=st.session_state['dish_description'],
         user_id=st.session_state['user_id'],
         is_logged_in=st.session_state['is_logged_in'],
         layout_position=track_new_meal_tab
     )
-    user_recommended_intake_df["result"] = result.get("login_or_create_account")
+    user_recommended_intake_df["result"] = save_meal_result.get("login_or_create_account")
+    st.session_state['save_meal_result'] = save_meal_result
+
+save_meal_result_persisted = st.session_state.get('save_meal_result')
+if save_meal_result_persisted.get("status") == 200:
+    storing_historical_data_result = save_meal_result_persisted.get("storing_historical_data_result")
+    if storing_historical_data_result:
+        storing_historical_data_message = storing_historical_data_result.get("message")
+        track_new_meal_tab.write(storing_historical_data_message)
 logging.info("-----------Finished get_user_confirmation_and_try_to_save_their_data-----------")
 
-
 # 5. Recommend dish.
-#### TODO: aggregate user_recommended_intake_df to make sure the actual_intake column is the daily actual intake
-#### now actual_intake might just be one meal's actual intake
+### TODO: aggregate user_recommended_intake_df to make sure the actual_intake column is the daily actual intake
+### now actual_intake might just be one meal's actual intake
 df_nutrient_data = user_recommended_intake_df.copy()
 
 #### TODO: remove this
