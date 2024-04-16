1895a5785d3b634cd140b19b0829d9fa8d2d9015
Michael <michael.yaputra@gmail.com>
2024-04-16 11:26:07 +1000

    fixed auth, merged master, ready for viz

--- core/sql/user_authentication.py
+++ core/sql/user_authentication.py
@@ -1,14 +1,4 @@
-create_user_profiles_query = """
-    CREATE OR REPLACE TABLE ilab.main.users (
-        user_id STRING,
-        gender STRING,
-        age FLOAT,
-        username  STRING,
-        password STRING
-    );
-"""
-
-create_user_profiles_query_2nd_version = """
+create_user_profiles_table = """
     CREATE OR REPLACE TABLE ilab.main.user_details (
         username VARCHAR,
         name STRING,
@@ -33,44 +23,8 @@ update_users_query = """
 SET password = CAST($password as STRING)
 WHERE username ilike CAST($username as STRING);
 """
+
 register_new_user_query = """
-    INSERT INTO {{ table_id }} BY NAME
-    (WITH
-        source_raw AS
-            (SELECT
-                $user_id AS user_id,
-                $gender AS gender,
-                $age AS age,
-                $username AS username,
-                $password AS password
-        )
-        ,source AS
-            (SELECT
-                CAST(user_id AS STRING) AS user_id,
-                CAST(gender AS STRING) AS gender,
-                CAST(age AS FLOAT) AS age,
-                CAST(username as STRING) as username,
-                CAST(password as STRING) AS password
-            FROM source_raw
-            )
-        ,target AS (
-            SELECT * FROM {{ table_id }}
-        )
-
-        ,records_to_insert AS (
-            SELECT
-                s.*
-            FROM source AS s
-            LEFT JOIN target AS t
-            ON s.user_id = t.user_id
-            WHERE t.user_id IS NULL
-        )
-
-        SELECT * FROM records_to_insert
-    );
-"""
-
-register_new_user_query_2nd_version = """
     INSERT INTO {{ table_id }}
         (WITH raw AS (
             SELECT
@@ -95,4 +49,4 @@ register_new_user_query_2nd_version = """
         SELECT *
         FROM source
     );
-            """
+"""
